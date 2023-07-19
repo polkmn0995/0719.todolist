1b9e7923580e51f090e59c5066db20d3776467ef
Choiyubin <polkmn0995@gmail.com>
2023-07-19 14:19:49 +0900

    개인 todolist 작업

---
+++ todo.js
@@ -0,0 +1,50 @@
+const btn = document.getElementById("btn");
+let addValue = document.getElementById("addValue");
+let result = document.getElementById("result");
+
+//할일 추가하기
+function addTodo() {
+  if (addValue.value === false) {
+    alert("내용을 입력하세요!!");
+  } else {
+    let list = document.createElement("li");
+    let del = document.createElement("button");
+    list.innerHTML = addValue.value;
+    result.appendChild(list);
+    list.appendChild(del);
+    del.innerText = "X";
+    del.style.fontSize = "20px";
+    del.style.border = "0";
+    del.style.float = "right";
+    del.style.right = "17px";
+    del.style.cursor = "pointer";
+    del.style.padding = "3px";
+    del.addEventListener("click", deleteList);
+    del.style.position = "relative";
+    addValue.value = "";
+    list.addEventListener("click", function () {
+      list.style.textDecoration = "line-through";
+      list.style.color = "black";
+      list.style.opacity = "0.5";
+    });
+  }
+}
+
+// 할 일 목록 삭제하기
+function deleteList(e) {
+  let removeOne = e.target.parentElement;
+  removeOne.remove();
+}
+
+// 모두 삭제하기
+function allClearlist(e) {
+  if (confirm("정말 삭제하시겠습니까?") == true) {
+    if (result.innerHTML == "") {
+      alert("삭제 할 목록이 없습니다.");
+    } else {
+      result.innerText = ""; // 전체 삭제
+    }
+  } else {
+    return false;
+  }
+}
